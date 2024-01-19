af967a3668bb6aeee950d58f03ce9454685044b8
STRAUBJACQUIN <titouan.straub@etu.univ-smb.fr>
2024-01-19 10:31:28 +0100

    Initial Commit

---
+++ ItemsTracker/ItemsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemsTrackerApp.swift
+//  ItemsTracker
+//
+//  Created by Titouan STRAUB on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ItemsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
